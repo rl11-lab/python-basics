e57e654397cb877f2d0c482112ea7c13555d0ee6
Rohit Lilhare <roliey511@gmail.com>
2026-03-18 15:19:48 +0530

    First upload of my expense tracker

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+st.set_page_config(page_title="Expense Tracker ",page_icon="Š")
+st.title(" Advanced Expense tracker ")
+st.write("""
+         Welcome to your personal expense tracker.
+         
+         use the sidebar widget:
+         
+         1. Add Expenses
+         2. view Expenses
+         3. Dashboard
+         
+         """)
+
+st.info("track your daily expenses easily")
